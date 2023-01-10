a78109e58c9f28c03979154d20c71100e5eec5d2
TolmaSh <m0v4anua@gmail.com>
2023-01-11 00:16:34 +0200

    feat:[49] create usersPage BLL

--- src/store/redux-store.ts
+++ src/store/redux-store.ts
@@ -1,10 +1,12 @@
 import {combineReducers, legacy_createStore as createStore} from "redux";
 import {profileReducer} from "./profileReducer";
 import {dialogsReducer} from "./dialogsReducer";
+import {usersReducer} from './usersReducer';
 
 const rootReducer = combineReducers({
     profilePage:profileReducer,
     dialogsPage:dialogsReducer,
+    usersPage: usersReducer
 })
 
 export type rootReducerType=ReturnType<typeof rootReducer>
---
+++ src/store/usersReducer.ts
@@ -0,0 +1,83 @@
+import {v1} from 'uuid';
+
+type UserType = {
+    id: string
+    follow: boolean
+    photoUrl: string
+    fullName: string
+    status: string
+    location: { country: string, city: string }
+}
+export type UsersReducerType = {
+    users: UserType[]
+}
+const initialState: UsersReducerType = {
+    users: [
+        {
+            id: v1(),
+            follow: true,
+            photoUrl: '',
+            fullName: 'Anatoly',
+            status: 'I am looking a Job right now..',
+            location: {country: 'Ukraine', city: 'Kiev'}
+        },
+        {
+            id: v1(),
+            follow: false,
+            photoUrl: '',
+            fullName: 'Vadym',
+            status: 'I am drink beer now',
+            location: {country: 'Ukraine', city: 'Kiev'}
+        },
+        {
+            id: v1(),
+            follow: true,
+            photoUrl: '',
+            fullName: 'Anastasiia',
+            status: 'I am sleep now',
+            location: {country: 'Ukraine', city: 'Kiev'}
+        },
+    ]
+}
+export const usersReducer = (state: UsersReducerType = initialState, action: UsersActionType) => {
+    switch (action.type) {
+        case 'FOLLOW-USER':
+            return {...state, users: state.users.map(u => u.id === action.payload.userID ? {...u, follow: true} : u)}
+        case 'UNFOLLOW-USER':
+            return {...state, users: state.users.map(u => u.id === action.payload.userID ? {...u, follow: false} : u)}
+        case 'SET-USERS':
+            return {...state, users: [...state.users, ...action.payload.users]}
+        default:
+            return state
+    }
+}
+
+
+type FollowUserActionType = ReturnType<typeof followUserAC>
+type UnFollowUserActionType = ReturnType<typeof unFollowUserAC>
+type SetUsersActionType = ReturnType<typeof setUsersAC>
+export type UsersActionType =
+    | FollowUserActionType
+    | UnFollowUserActionType
+    | SetUsersActionType
+
+
+export const followUserAC = (userID: string) => {
+    return {
+        type: 'FOLLOW-USER',
+        payload: {userID}
+    } as const
+}
+export const unFollowUserAC = (userID: string) => {
+    return {
+        type: 'UNFOLLOW-USER',
+        payload: {userID}
+    } as const
+}
+
+export const setUsersAC = (users: UserType[]) => {
+    return {
+        type: 'SET-USERS',
+        payload: {users}
+    } as const
+}
