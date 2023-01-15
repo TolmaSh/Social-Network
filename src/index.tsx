b87a68b28fd57742a02239c04c6dd51fd22b5a13
TolmaSh <m0v4anua@gmail.com>
2023-01-15 22:38:22 +0200

    feat:upgrade react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './store/redux-store';
 import {Provider} from 'react-redux';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <App/>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
